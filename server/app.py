254b20c6ddc71599f7b7b46d7bcb47987bf6ff62
Mark Coats <alexco1234@gmail.com>
2023-04-11 11:52:08 -0500

    wtf with last test

--- server/app.py
+++ server/app.py
@@ -20,19 +20,72 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    return_list = []
+    for business in Bakery.query.all():
+        bak = {
+            'id' : business.id,
+            'name' : business.name,
+            'created_at' : business.created_at
+        }
+        return_list.append(bak)
+
+    response = make_response(
+        return_list,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bak = Bakery.query.filter(Bakery.id == id).first()
+    ret = {
+        'id':bak.id,
+        'name':bak.name,
+        'created_at':bak.created_at
+    }
+    response = make_response(
+        ret,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    return_list = []
+    for goods in BakedGood.query.order_by(BakedGood.price).all():
+        good = {
+            'id' : goods.id,
+            'name' : goods.name,
+            'created_at' : goods.created_at,
+            'price':goods.price
+        }
+        return_list.append(good)
+
+    response = make_response(
+        return_list,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    bak=BakedGood.query.order_by(db.desc(BakedGood.price)).first()
+    good = {
+        'id' : bak.id,
+        'name' : bak.name,
+        'created_at' : bak.created_at,
+        'price':bak.price
+    }
+
+    response = make_response(
+        good,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
@@ -11,10 +12,22 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    #serialize_rules = ('-baked_goods.bakery')
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    baked_goods = db.relationship('BakedGood', backref='bakery')
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods')
+
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
